718dcae29f373adf698e64181b8850d0faa96d49
simokitkat <simokitkat@yahoo.com>
2024-05-23 12:52:49 +0300

    feat: add animation and adaptivness to the projects page

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,24 +1,81 @@
+import { motion } from "framer-motion";
 import { certificatesData } from "../../utils/CertificatesData";
 import Certificate from "./Certificate";
 import "./certifications.scss";
+
+const certificatesH2Variants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.5,
+    },
+  },
+};
+
+const certificatesPVariants = {
+  hidden: {
+    x: "100vw",
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.5,
+    },
+  },
+};
+
+const certificatesGridVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      delay: 2,
+    },
+  },
+};
 
 export default function Certifications() {
   return (
     <section className="certifications">
       <div className="container">
         <div className="text">
-          <h2>Certificates</h2>
-          <p>
+          <motion.h2
+            variants={certificatesH2Variants}
+            initial="hidden"
+            animate="visible"
+          >
+            Certificates
+          </motion.h2>
+          <motion.p
+            variants={certificatesPVariants}
+            initial="hidden"
+            animate="visible"
+          >
             Although I believe that the ability to code is what truly matters,
             I&apos;m proud to showcase these certificates as a testament to my
             dedication and perseverance in continuously improving my skills.
-          </p>
+          </motion.p>
         </div>
-        <div className="certifications-grid">
+        <motion.div
+          className="certifications-grid"
+          variants={certificatesGridVariants}
+          initial="hidden"
+          animate="visible"
+        >
           {certificatesData.map((certificate) => {
             return <Certificate {...certificate} key={certificate.name} />;
           })}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,81 @@
+import { motion } from "framer-motion";
 import { projectsData } from "../../utils/ProjectsData";
 import Project from "./Project";
 import "./projects.scss";
+
+const projectsH2Variants = {
+  hidden: {
+    y: "-100vh",
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.5,
+    },
+  },
+};
+
+const projectsPVariants = {
+  hidden: {
+    y: "100vh",
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      delay: 0.5,
+    },
+  },
+};
+
+const projectsGridVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      delay: 2,
+    },
+  },
+};
+
 export default function Projects() {
   return (
     <section className="projects">
       <div className="container">
         <div className="text">
-          <h2>Projects</h2>
-          <p>
+          <motion.h2
+            variants={projectsH2Variants}
+            initial="hidden"
+            animate="visible"
+          >
+            Projects
+          </motion.h2>
+          <motion.p
+            variants={projectsPVariants}
+            initial="hidden"
+            animate="visible"
+          >
             Here are some of my favorite projects, showcasing my skills and
             creativity in coding. Each project is a reflection of my dedication
             to crafting innovative solutions that make a difference.
-          </p>
+          </motion.p>
         </div>
-        <div className="projects-grid">
+        <motion.div
+          className="projects-grid"
+          variants={projectsGridVariants}
+          initial="hidden"
+          animate="visible"
+        >
           {projectsData.map((project) => {
             return <Project {...project} key={project.name} />;
           })}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
